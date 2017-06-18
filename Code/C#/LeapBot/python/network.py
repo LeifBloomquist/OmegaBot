b75f1d0aabe422854694937a8b62a65f7b37e290
Leif Bloomquist <github@leifbloomquist.net>
2017-06-18 00:39:33 -0400

    Added steering adjustment

--- Code/C#/LeapBot/python/network.py
+++ Code/C#/LeapBot/python/network.py
@@ -2,7 +2,9 @@
 import time
 import socket
 
-port = 2000
+PORT = 2000
+STEERING_ADJUST = 1.7
+
 
 def Initialize():
   pwmExp.driverInit()
@@ -17,29 +19,28 @@ def Stop():
 def SetLeftRight( speed_left, speed_right):
    "This moves the robot forward with speeds for left and right"
    pwmExp.setupDriver(7, speed_left, 0)
-   pwmExp.setupDriver(8, speed_right, 0)
+   pwmExp.setupDriver(8, speed_right * STEERING_ADJUST, 0)
    return
 
 Initialize()
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.settimeout(5.0)
-s.bind(("", port))
+s.settimeout(2.0)
+s.bind(("", PORT))
 
-print "Waiting on port: ", port
+print "Waiting on port: ", PORT
+
 
 while True:
 
     try:
        data, addr = s.recvfrom(100)
-       print "Received: ", data
        leftright = data.split(",")
        left = int(leftright[0])
        right = int(leftright[1])
-       print "   Left: ", left
-       print "   Right: ", right
        SetLeftRight(left, right)
+       print "Received: ", data
 
     except socket.timeout:
        print "Receive timed out"
@@ -49,3 +50,5 @@ while True:
        print
        print "Data format error"
        Stop()
+
+
